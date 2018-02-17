80a5482d0b2a0748ffe886b5ac0a7dc1591c2b57
Chandrahas Kondle <30701318+chandrahaskondle@users.noreply.github.com>
2018-02-17 17:58:44 +0530

    Create bubbleSort.java

---
+++ bubbleSort.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        int i,j;
+        while(T>0){
+            int temp=0;
+            int N=sc.nextInt();
+            int a[]=new int[N];
+            for(i=0;i<N;i++)
+                a[i]=sc.nextInt();
+            for(i=0;i<N-1;i++){
+                for(j=0;j<N-i-1;j++){
+                    if(a[j]>a[j+1]){
+                        int t=a[j];
+                        a[j]=a[j+1];
+                        a[j+1]=t;
+                        temp++;
+                    }   
+                }
+                if(temp==0)break;
+            }
+            System.out.println(temp);
+            T--;
+       }
+    }
+}

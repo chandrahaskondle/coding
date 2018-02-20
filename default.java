e7b33a172b2830f3e74fd99a525e0f68a7d895a0
Chandrahas Kondle <30701318+chandrahaskondle@users.noreply.github.com>
2018-02-20 10:47:48 +0530

    Update default.java

--- default.java
+++ default.java
@@ -2,15 +2,13 @@
 
 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 int t=Integer.parseInt(br.readLine());
-for(int k=0;k<t;k+=){
+for(int k=0;k<t;k++){
   int n=Integer.parseInt(br.readLine());
   String str[]=br.readLine.split(" ");
   int arr[]=new int[n];
   for(int i=0;i<n;i++)
-    arr[i]=Integer.parseInt(str[k]);
+    arr[i]=Integer.parseInt(str[i]);
     
     
-    
-    
-  t--;
 }
+   

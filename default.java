362b5cfb245267c9dd2f47578d639baf4bacf0bb
Chandrahas Kondle <30701318+chandrahaskondle@users.noreply.github.com>
2018-02-20 10:41:40 +0530

    Create default.java

---
+++ default.java
@@ -0,0 +1,16 @@
+//default hackerrank code for testcases 't' and inputValue 'n'
+
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+int t=Integer.parseInt(br.readLine());
+for(int k=0;k<t;k+=){
+  int n=Integer.parseInt(br.readLine());
+  String str[]=br.readLine.split(" ");
+  int arr[]=new int[n];
+  for(int i=0;i<n;i++)
+    arr[i]=Integer.parseInt(str[k]);
+    
+    
+    
+    
+  t--;
+}

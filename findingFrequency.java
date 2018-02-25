e38b8427739c52dece999d85e443479fc11b37eb
Chandrahas Kondle <30701318+chandrahaskondle@users.noreply.github.com>
2018-02-25 13:08:35 +0530

    Update findingFrequency.java

--- findingFrequency.java
+++ findingFrequency.java
@@ -8,9 +8,9 @@ public class Solution {
 
     public static void main(String[] args) throws IOException {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        BufferedReader br=new BufferReader(new InputStreamReader(System.in));
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
-        String str=br.readLine().split(" ");
+        String[] str=br.readLine().split(" ");
         HashMap<Integer,Integer> mp=new HashMap<Integer,Integer>();
         for(int k=0;k<n;k++){
             int m=Integer.parseInt(str[i]);

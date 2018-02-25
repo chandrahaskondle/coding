67877fa23a78cc4c0094229a7cd52db7b6341e40
Chandrahas Kondle <30701318+chandrahaskondle@users.noreply.github.com>
2018-02-25 13:03:08 +0530

    Update findingFrequency.java

--- findingFrequency.java
+++ findingFrequency.java
@@ -6,31 +6,28 @@ import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args)throws IOException 
-    {
-      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        BufferedReader br=new BufferReader(new InputStreamReader(System.in()));
         int n=Integer.parseInt(br.readLine());
-        String []str=br.readLine().split(" ");
+        String str=br.readLine().split(" ");
         HashMap<Integer,Integer> mp=new HashMap<Integer,Integer>();
-        for(int i=0;i<n;i++)
-        {
-            int x=Integer.parseInt(str[i]);
-            if(mp.containsKey(x))
-            {
-                int z=mp.get(x);
-                mp.put(x,z+1);
+        for(int k=0;k<n;k++){
+            int m=Integer.parseInt(str[i]);
+            if(mp.containsKey(m)){
+                int temp=mp.get(m);
+                mp.put(m,temp+1);
             }
             else
-                mp.put(x,1);
+                mp.put(m,1);
         }
-         int t=Integer.parseInt(br.readLine());
-        for(int i=0;i<t;i++)
-        {
-             int k=Integer.parseInt(br.readLine());
-            if(mp.get(k) == null)
-             System.out.println(0);   
-            else 
-                System.out.println((mp.get(k)));
+        int q=Integer.parseInt(br.readLine());
+        for(int i=0;i<q;i++){
+            int temp=Integer.parseInt(br.readLine());
+            if(mp.get(temp)==null)
+                System.out.println("0");
+            else
+                System.out.println(mp.get(temp));
         }
     }
 }

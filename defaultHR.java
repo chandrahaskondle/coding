26fbb1405a4f0b232484696a7357bfc898823c14
Chandrahas Kondle <30701318+chandrahaskondle@users.noreply.github.com>
2018-03-03 12:50:00 +0530

    Update defaultHR.java

--- defaultHR.java
+++ defaultHR.java
@@ -11,13 +11,14 @@ public class Solution {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int t=Integer.parseInt(br.readLine());
         for(int k=0;k<t;k++){
-            int n=Integer.parseInt(br.readLine());
-            int k=Integer.parseInt(br.readLine());
+            String[] s=br.readLine().split(" ");
+            int n=Integer.parseInt(s[0]);
+            int key=Integer.parseInt(s[1]);
             String[] str=br.readLine().split(" ");
             int[] arr=new int[n];
             for(int i=0;i<n;i++)
                 arr[i]=Integer.parseInt(str[i]);
-            
+                     
         }
     }
 }

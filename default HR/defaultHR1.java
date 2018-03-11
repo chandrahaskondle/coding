d358abbdd81edec6001936c630ef293a42b65766
Chandrahas Kondle <30701318+chandrahaskondle@users.noreply.github.com>
2018-03-11 17:03:30 +0530

    Create defaultHR1.java

---
+++ default HR/defaultHR1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        String s1[]=br.readLine().split(" ");
+        int n=Integer.parseInt(s1[0]);
+        int key=Integer.parseInt(s1[1]);
+        String s2[]=br.readLine().split(" ");
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+            arr[i]=Integer.parseInt(s2[i]);
+        for(int k=0;k<t;k++){
+        
+        
+        
+        }
+    }
+}

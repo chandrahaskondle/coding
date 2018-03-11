c66b6aca6aa04a1a4f5b4f6951b9e8823dbc9c68
Chandrahas Kondle <30701318+chandrahaskondle@users.noreply.github.com>
2018-03-11 19:25:28 +0530

    Create bufferWriter.java

---
+++ default HR/bufferWriter.java
@@ -0,0 +1,10 @@
+
+BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+StringBuilder sb=new StringBuilder();
+for(int k=0;k<t;k++){
+
+    sb.append("ans\n");
+
+}
+bw.Write(sb.toString());
+bw.close();

981513340d386ea831e2b01a252763e2701937bf
Chandrahas Kondle <30701318+chandrahaskondle@users.noreply.github.com>
2018-03-11 17:07:20 +0530

    Create defaultHR2.java

---
+++ default HR/defaultHR2.java
@@ -0,0 +1,7 @@
+
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+int t=Integer.parseInt(br.readLine());
+for(int k=0;k<t;k++){
+  int n=Integer.parseInt(br.readLine());
+
+    

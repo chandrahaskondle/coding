f274f190516188e7aca3c9081ae9aca1b68e9988
Chandrahas Kondle <30701318+chandrahaskondle@users.noreply.github.com>
2018-03-11 21:32:02 +0530

    Update defaultHR2.java

--- default HR/defaultHR2.java
+++ default HR/defaultHR2.java
@@ -1,7 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-int t=Integer.parseInt(br.readLine());
-for(int k=0;k<t;k++){
-  int n=Integer.parseInt(br.readLine());
-
-    
+public class Solution {
+    public static void main(String[] args) {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        for(int k=0;k<t;k++){
+            int n=Integer.parseInt(br.readLine());
+            
+            
+        }
+    }
+}

e74cb55476ed6c0f02fa6476f1dab6ebddb6a20d
Chandrahas Kondle <30701318+chandrahaskondle@users.noreply.github.com>
2018-03-11 18:55:47 +0530

    Update input1.java

--- default HR/input1.java
+++ default HR/input1.java
@@ -9,17 +9,15 @@ public class Solution {
     {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int t=Integer.parseInt(br.readLine());
-        String s1[]=br.readLine().split(" ");
-        int n=Integer.parseInt(s1[0]);
-        int key=Integer.parseInt(s1[1]);
-        String s2[]=br.readLine().split(" ");
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++)
-            arr[i]=Integer.parseInt(s2[i]);
         for(int k=0;k<t;k++){
-        
-        
-        
+            String s1[]=br.readLine().split(" ");
+            int n=Integer.parseInt(s1[0]);
+            int key=Integer.parseInt(s1[1]);
+            String s2[]=br.readLine().split(" ");
+            int arr[]=new int[n];
+            for(int i=0;i<n;i++)
+                arr[i]=Integer.parseInt(s2[i]);
+                     
         }
     }
 }

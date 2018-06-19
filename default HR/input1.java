3f2527873c198f057f013ebcfe8876feb45b5556
chandrahas_k <30701318+chandrahaskondle@users.noreply.github.com>
2018-06-19 17:45:33 +0530

    Update input1.java

--- default HR/input1.java
+++ default HR/input1.java
@@ -1,18 +1,17 @@
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException
     {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OuputStreamWriter(System.out));
+        StringBuilder sb=new StringBuilder();
         int t=Integer.parseInt(br.readLine());
         for(int k=0;k<t;k++){
-            String str1[]=br.readLine().split(" ");
-            int n=Integer.parseInt(str1[0]);
-            int key=Integer.parseInt(str1[1]);
+            String str[]=br.readLine().split(" ");
+            int n=Integer.parseInt(str[0]);
+            int key=Integer.parseInt(str[1]);
             String str2[]=br.readLine().split(" ");
             int arr[]=new int[n];
             for(int i=0;i<n;i++)

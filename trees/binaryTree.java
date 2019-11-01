86cba4b9cf3b118ff0309416b767cda5c8e4fba4
chandrahas_k <30701318+chandrahaskondle@users.noreply.github.com>
2019-11-02 00:39:24 +0530

    Update binaryTree.java

--- trees/binaryTree.java
+++ trees/binaryTree.java
@@ -1,76 +1,56 @@
-// java code implementing binaryTree with some important functions ( insertion, deletion, searching )
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class BinaryTree{
+class BinarySearchTree{
     private static Node root;
-	private static class Node{
-		int data;
-		Node left,right;
-		Node(int data){
-			this.data=data;
-			this.left=null;
-			this.right=null;
-		}
-	}
-	
-	public void PreOrder(Node curr){
-	    System.out.print(curr.data+" ");
-	    if(curr.left!=null)
-	        PreOrder(curr.left);
-	    if(curr.right!=null)
-	        PreOrder(curr.right);
-	}
-	
-	public void InOrder(Node curr){
-	    if(curr.left!=null)
-	        InOrder(curr.left);
-	    System.out.print(curr.data+" ");
-	    if(curr.right!=null)
-	        InOrder(curr.right);
-	}
-	
-	public void PostOrder(Node curr){
-	    if(curr.left!=null)
-	        PostOrder(curr.left);
-	    if(curr.right!=null)
-	        PostOrder(curr.right);
-	    System.out.print(curr.data+" ");
-	}
-	
-	public void BFS(Node curr){
-	    Queue<Node> queue=new LinkedList<Node>();
-	    queue.add(root);
-	    while(!queue.isEmpty()){
-	        Node curr_node=queue.remove();
-	        System.out.print(curr_node.data+" ");
-	        if(curr_node.left!=null)
-	            queue.add(curr_node.left);
-	        if(curr_node.right!=null)
-	            queue.add(curr_node.right);
-	    }
-	}
-	
-	public static void main(String args[]){
-		root=new Node(1);
-		root.left=new Node(2);
-		root.right=new Node(3);
-		Node curr_node=root.left;
-		curr_node.left=new Node(4);
-		curr_node.right=new Node(5);
-		curr_node=root.right;
-		curr_node.left=new Node(6);
-		curr_node.right=new Node(7);
-		BinaryTree binaryTree=new BinaryTree();
-		binaryTree.PreOrder(root);
-		System.out.println();
-		binaryTree.InOrder(root);
-		System.out.println();
-		binaryTree.PostOrder(root);
-		System.out.println();
-		binaryTree.BFS(root);
-	}
-	
+    private static class Node{
+        int data;
+        Node left,right;
+        Node(int data){
+            this.data=data;
+            this.left=this.right=null;
+        }
+    }
+    
+    public void add(int key){
+        if(root==null)
+            root=new Node(key);
+        else{
+            Node temp=root;
+            while( true ){
+                if(key < temp.data){
+                    if(temp.left==null){
+                        temp.left=new Node(key);
+                        break;
+                    }
+                    temp=temp.left;
+                }
+                else{
+                    if(temp.right==null){
+                        temp.right=new Node(key);
+                        break;
+                    }
+                    temp=temp.right;
+                }
+            }
+        }
+    }
+    
+    public void inorder(Node root){
+        if(root == null)
+            return;
+        inorder(root.left);
+        System.out.println(root.data);
+        inorder(root.right);
+    }
+    
+    public static void main(String args[]){
+        int arr[]={11, 6, 8, 19, 4, 10, 5, 17, 43, 49, 31};
+        BinarySearchTree bst=new BinarySearchTree();
+        for(int i=0;i<arr.length;i++)
+            bst.add(arr[i]);
+        bst.inorder(root);
+    }
+    
 }

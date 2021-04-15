ec3c04c0a7b1aa444303425b51abbe8c6e9ce178
chandrahas_k <30701318+chandrahaskondle@users.noreply.github.com>
2021-04-15 19:03:42 +0530

    Update read and write from file.java

--- read and write from file.java
+++ read and write from file.java
@@ -2,22 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-  private final Scanner scanner;
-  public static void main(String args[]){
-    scanner = new Scanner(System.in);
-    String inputFilename = scanner.nextLine();
-    File outputFile = new File("result- "+filename);
-    if (!outputFile.exists()) {
-	    outputFile.createNewFile();
-	  }
-    BufferedReader br = new BufferedReader(new FileReader(inputFilename));
-    BufferedWriter bw = new BufferedReader(new FileWriter(outputFile));
-    while( (eachLine = br.readLine()) != null){
-      // copy file contents to new file
-      bw.write(eachLine);
-      bw.newLine();
-    }
-    br.close();
-    bw.close();
-  }
+
+	public static void main(String[] args) throws IOException {
+	    String inputFilename = "src/myStudyApp/test.txt";
+	   
+	    BufferedReader br = new BufferedReader(new FileReader(inputFilename));
+	    
+	    File outputFile = new File("result.txt");
+	    if (!outputFile.exists()) {
+		  	outputFile.createNewFile();
+	    }
+	    BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile));
+		
+	    // copy file contents to new file 
+			String eachLine;
+	    while( (eachLine = br.readLine()) != null){
+	      bw.write(eachLine);
+	      bw.newLine();
+	      System.out.println(eachLine);
+	    }
+	    br.close();
+	    bw.close();
+	}
 }
